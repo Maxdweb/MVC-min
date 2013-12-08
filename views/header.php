f59e423f5d8c0e453210811a95b7bbd16fc24f9e
Maxdweb <deleuil.maxime@gmail.com>
2013-12-08 18:12:40 +0100

    update home views

--- views/header.php
+++ views/header.php
@@ -1,12 +1,3 @@
 <div id="header">
-	<h1>Maxime DELEUIL WEBER</h1>
-	<h3>Etudiant à l'IUP MIAGE d'Aix en Provence</h3>
-	<div id="menu">
-		<ul>
-			<li><a href="<?php echo WEBROOT; ?>home">Accueil</a></li>
-			<li><a href="<?php echo WEBROOT; ?>Skills">Compétences</a></li>
-			<li><a href="<?php echo WEBROOT; ?>Portfolio">Portfolio</a></li>
-			<li><a href="<?php echo WEBROOT; ?>Blog">Blog</a></li>
-		</ul>
-	</div>
+	<h1>MVC-MIN</h1>
 </div>
--- views/home.php
+++ views/home.php
@@ -1,11 +1,3 @@
 <div id="content">
-	<div id="coordsBar">
-			<img src="assets/img/portrait_min.jpg" alt="Portrait" class="portrait"/>
-		<div class="side">
-			<a href="http://www.viadeo.com/fr/profile/maxime.deleuil-weber"><img src="assets/img/viadeo_min.png"/></a>
-			<a href="http://www.linkedin.com/pub/maxime-deleuil/69/8bb/192"><img src="assets/img/linkedin_min.png"/></a><br/>
-			<a href="plus.google.com/107234184815244763235"><img src="assets/img/google_steel_min.png"/></a>
-			<a href="https://twitter.com/MaxDWeb"><img src="assets/img/twitter_min.png"/></a>
-		</div>
-	</div>
+	<p>MVC-min, le framework léger pour un développement rapide !</p>
 </div>
